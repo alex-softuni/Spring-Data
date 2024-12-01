b29b51a477757f20ad4e3d23ec4708622f7d3164
worldwidehandsomeJT <alexander_0@abv.bg>
2024-12-01 19:58:55 +0100

    Solved problem added

--- Intro-to-Hibernate-Ex/src/main/java/Main.java
+++ Intro-to-Hibernate-Ex/src/main/java/Main.java
@@ -1,14 +1,20 @@
+import entities.Town;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.EntityManagerFactory;
 import jakarta.persistence.Persistence;
+
 
 public class Main {
     public static void main(String[] args) {
         EntityManagerFactory emf = Persistence.createEntityManagerFactory("soft_uni");
         EntityManager em = emf.createEntityManager();
         em.getTransaction().begin();
-
+        getTownsSizeBiggerThanFive(em);
         em.getTransaction().commit();
         em.close();
+    }
+
+    private static void getTownsSizeBiggerThanFive(EntityManager em) {
+        em.createQuery("FROM Town WHERE length(name) > 5", Town.class).getResultStream().forEach(town -> System.out.printf("%s%n",town.getName()));
     }
 }
