9bcfc3e60d87abc93d5aa7da78c5c062ca36b584
worldwidehandsomeJT <alexander_0@abv.bg>
2025-01-13 17:04:25 +0100

    setup config successfully

---
+++ Hibernate-Code-First-Lab/src/main/java/Main.java
@@ -0,0 +1,13 @@
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("vehicle_hierarchy");
+        EntityManager entityManager = emf.createEntityManager();
+        entityManager.getTransaction().begin();
+
+        entityManager.close();
+    }
+}
