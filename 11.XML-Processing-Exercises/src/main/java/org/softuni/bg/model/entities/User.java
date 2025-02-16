85eefbf46cc56ac895e3ea61d64bd73299e5332d
worldwidehandsomeJT <alexander_0@abv.bg>
2025-02-16 23:15:08 +0200

    structure complete.

---
+++ 11.XML-Processing-Exercises/src/main/java/org/softuni/bg/model/entities/User.java
@@ -0,0 +1,17 @@
+package org.softuni.bg.model.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "users")
+public class User extends BaseEntity {
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+    private int age;
+}
